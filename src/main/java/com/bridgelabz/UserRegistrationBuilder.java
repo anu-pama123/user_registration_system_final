fb9d7d61175fa8f4bd8c5b10aae5c5dc858d5046
anupama cv <anu.anupamacv@gmail.com>
2021-05-30 18:04:27 +0530

    ADDED: welcome message

---
+++ src/main/java/com/bridgelabz/UserRegistrationBuilder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class UserRegistrationBuilder
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to user registration system");
+    }
+}
