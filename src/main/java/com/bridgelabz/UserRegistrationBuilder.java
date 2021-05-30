879e75ef00c87357226b3e6ce9b5756354ee465e
anupama cv <anu.anupamacv@gmail.com>
2021-05-30 18:10:05 +0530

    ADDED: functionality to add valid first name using regex pattern

--- src/main/java/com/bridgelabz/UserRegistrationBuilder.java
+++ src/main/java/com/bridgelabz/UserRegistrationBuilder.java
@@ -1,9 +1,40 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class UserRegistrationBuilder
 {
+    public static boolean checkPattern(String input, String patternNum)
+    {
+        Pattern pattern = Pattern.compile(patternNum);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find())
+        {
+            return true;
+        }
+        return false;
+    }
+    static void outputChecker(boolean checkPattern)
+    {
+        if(checkPattern==true)
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+            System.out.println("InValid");
+        }
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to user registration system");
+        Scanner name;
+        String input;
+        System.out.println("Enter your name(Should start with capital letter and has minimum 3 character) : ");
+        name = new Scanner(System.in);
+        input = name.nextLine();
+        String pattern = "^[A-Z]{1}+[a-z]{2,}$";
+        outputChecker(checkPattern(input, pattern));
     }
 }
